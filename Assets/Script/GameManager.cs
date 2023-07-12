70275f0e7be3420674a4c7951338da9664bb3085
ekqls07 <01033361687lg@gmail.com>
2023-07-12 17:36:39 +0900

    project

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public int killScore = 0;
+    public Text killText;
+
+    public GameObject food;
+    public GameObject dog;
+    public GameObject cat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+        InvokeRepeating("MakeFood", 0f, 0.2f);
+        InvokeRepeating("MakeCat", 0f, 1f);
+    }
+
+    void MakeCat()
+    {
+        Instantiate(cat);
+    }
+
+
+    void MakeFood()
+    {
+        float x = dog.transform.position.x;
+        float y = dog.transform.position.y + 0.2f;
+        Instantiate(food, new Vector3(x, y, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        killText.text = killScore.ToString();
+    }
+}
